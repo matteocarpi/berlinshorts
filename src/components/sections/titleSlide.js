47327c932bc82bb6930f7749fef5ad10703ccd82
Matteo Carpi <matteocarpi94@gmail.com>
2019-09-06 12:05:07 +0200

    styling of title slide

--- src/components/sections/titleSlide.js
+++ src/components/sections/titleSlide.js
@@ -1,7 +1,13 @@
 import React from "react";
+
+import classnames from 'classnames';
+
 import logo from '../../assets/img/Fox-logo.png';
 import title from "../../assets/img/Berlin-shorts-logo-White.svg";
 import date from "../../assets/img/OCTOBER-2019White.svg";
+
+import styles from '../../assets/styles/TitleSlide.module.scss';
+import universal from '../../assets/styles/Universal.module.scss';
 
 class TitleSlide extends React.Component {
   render() {
@@ -15,8 +21,7 @@ class TitleSlide extends React.Component {
             <div className="title">
 
               <img src={title} alt=""/>
-              <img className="date" src={date} alt=""/>
-
+              <span className={classnames(styles.date, universal.white)}>OCTOBER 2019</span>
             </div>
 
           </div>
