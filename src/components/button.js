38b74f03769113a3d5755ea545a9fe3524fa9c68
Matteo Carpi <matteocarpi94@gmail.com>
2019-09-07 17:37:52 +0200

    fixing button

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 class Button extends React.Component {
   render (props) {
     return (
-      <a className={this.props.className} target={this.props.target} href={this.props.link}>{this.props.text}</a>
+      <Link className={this.props.className} target={this.props.target} to={this.props.link}>{this.props.text}</Link>
     )
   }
 }
