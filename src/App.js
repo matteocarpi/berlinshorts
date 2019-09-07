c2e7869fbb0309d2d1ca697c85d86a6dfb00a2aa
Matteo Carpi <matteocarpi94@gmail.com>
2019-09-07 13:42:42 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Footer from './components/footer';
 import Navigation from './components/navigation';
 import Home from "./components/pages/home";
 import Concept from "./components/pages/concept";
@@ -12,12 +13,11 @@ function App() {
         <div className="App">
           <Navigation />
 
+          <Route exact path="/" component={Home} />
 
-
-            <Route exact path="/" component={Home} />
-
-            <Route exact path="/concept" component={Concept} />
-
+          <Route exact path="/concept" component={Concept} />
+          
+          <Footer />
         </div>
       </Switch>
     </Router>
---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import universal from '../assets/styles/Universal.module.scss';
+
+class Footer extends React.Component {
+  render() {
+    return <footer>Copyright©2019 Il Varco</footer>;
+  }
+}
+
+export default Footer;
